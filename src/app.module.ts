66556313e64acad5b88a67d8110ca12b39306655
Rustem <rustem_hajrullin@enot.work>
2025-03-11 01:57:12 +0300

    fix: configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
-import { UploadModule } from './upload/upload.module';
 import { AuthModule } from './auth/auth.module';
+import { WishlistModule } from './wishlist/wishlist.module';
+import { MulterModule } from '@nestjs/platform-express';
 import { DatabaseModule } from './database/database.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     DatabaseModule,
     UserModule,
-    UploadModule,
     AuthModule,
+    WishlistModule,
+    UploadModule,
+    MulterModule.register({
+      dest: './uploads',
+    }),
   ],
 })
 export class AppModule {}
