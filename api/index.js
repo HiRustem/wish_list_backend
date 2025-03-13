b08285d2c99d06b83c65deab3f71ab8fb840d456
Rustem <rustem_hajrullin@enot.work>
2025-03-13 09:47:35 +0300

    fix: delete builds

--- api/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const AppFactory_1 = __importDefault(require("../src/AppFactory"));
-exports.default = AppFactory_1.default.create().expressApp;
